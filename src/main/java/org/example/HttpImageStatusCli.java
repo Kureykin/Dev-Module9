5b7ca021ac1c5bb98887347da4cf1dc66f2ada3a
Kureykin <148269382+Kureykin@users.noreply.github.com>
2026-03-27 16:49:50 +0100

    Update HttpImageStatusCli.java

--- src/main/java/org/example/HttpImageStatusCli.java
+++ src/main/java/org/example/HttpImageStatusCli.java
@@ -22,6 +22,8 @@ public class HttpImageStatusCli {
             endOfInput = true;
         }
 
+        scan.close();
+
         try {
             new HttpStatusImageDownloader().downloadStatusImage(statusCode);
         } catch (PostNotFoundException e) {
